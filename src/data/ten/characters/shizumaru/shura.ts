6a62fa72c73e73194c9bec3013a36f38e4f81084
Qrac <qrac.jp@gmail.com>
2024-09-18 11:53:41 +0900

    Fix data

--- src/data/ten/characters/shizumaru/shura.ts
+++ src/data/ten/characters/shizumaru/shura.ts
@@ -24,7 +24,7 @@ export const tenShizumaruShura: DataItem = {
           command: "↓↘→+D",
         },
         {
-          name: "緋刀流 真・雨流狂落斬",
+          name: "緋刀流 真・雨流れ狂落斬",
           command: "D継続押し後離す",
         },
       ],
