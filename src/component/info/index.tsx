a18a330c5ca524c5d538d296aba74c636f045a9c
Qrac <qrac.jp@gmail.com>
2025-03-08 11:53:28 +0900

    Remove tags

--- src/component/info/index.tsx
+++ src/component/info/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentCommand, ComponentTags } from "../skills"
+import { ComponentCommand } from "../skills"
 import "./index.css"
 
 export function ComponentInfo() {
@@ -35,18 +35,6 @@ export function ComponentInfo() {
         <div className="info-item">
           <dt className="info-item-head">G不能</dt>
           <dd className="info-item-body">ガード不能攻撃</dd>
-        </div>
-        <div className="info-item">
-          <dt className="info-item-head">
-            <ComponentTags tags={[{ name: "怒", color: 2 }]} />
-          </dt>
-          <dd className="info-item-body">怒り時に強化</dd>
-        </div>
-        <div className="info-item">
-          <dt className="info-item-head">
-            <ComponentTags tags={[{ name: "素+", color: 4 }]} />
-          </dt>
-          <dd className="info-item-body">素手状態でも使用可</dd>
         </div>
       </dl>
       <p className="info-note">※コマンドはすべてキャラクターが右向きの場合</p>
